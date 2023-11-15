447f97276fcb14b9ab86e155386da636fa51b5ec
Juan Archidona <105814329+JuanArchidona@users.noreply.github.com>
2023-11-15 09:28:57 +0100

    Update test_bicimad.py

--- 01. Programación avanzada en Python/prueba_entrega_final/tests/test_bicimad.py
+++ 01. Programación avanzada en Python/prueba_entrega_final/tests/test_bicimad.py
@@ -1,60 +1,70 @@
 import pytest
-from bicimad.bicimad import BiciMad
+from bicimad import BiciMad
 import pandas as pd
 from io import StringIO
 
-
-# Datos de prueba en formato CSV
-CSV_TEST_DATA = """unlock_date;idBike;fleet;trip_minutes;geolocation_unlock;address_unlock;lock_date;station_lock;lock_station_name;unlock_station_name;...
-2021-06-01 07:00;123;1;15;POINT(-3.70379 40.416775);Calle de Alcalá;2021-06-01 07:15;42;Estación 42;Estación A;...
-2021-06-01 07:10;456;1;30;POINT(-3.70379 40.416775);Gran Vía;2021-06-01 07:40;58;Estación 58;Estación B;...
+# Mock data para pruebas
+MOCK_CSV_DATA = """unlock_date,trip_minutes,unlock_station_name,station_unlock,fleet,idBike,lock_date,station_lock
+2021-06-01 08:00,30,Estación 1,1,100,1234,2021-06-01 08:30,2
+2021-06-02 09:00,45,Estación 2,2,100,5678,2021-06-02 09:45,3
 """
 
 @pytest.fixture
-def bicimad_instance():
-    """
-    Proporciona una instancia de la clase BiciMad preparada con datos de prueba para junio de 2021.
-    La llamada al método estático `get_data` se simula para que devuelva un DataFrame basado en CSV_TEST_DATA.
-    """
-    BiciMad.get_data = staticmethod(lambda month, year: pd.read_csv(StringIO(CSV_TEST_DATA), sep=';', parse_dates=['unlock_date', 'lock_date']))
+def mock_csv_data():
+    return StringIO(MOCK_CSV_DATA)
+
+@pytest.fixture
+def bicimad_instance(mock_csv_data):
+    # Mock la función get_csv para devolver los datos mock
+    BiciMad.get_data = lambda self, month, year: pd.read_csv(
+        mock_csv_data,
+        parse_dates=['unlock_date', 'lock_date'],
+        index_col='unlock_date'
+    )
     return BiciMad(6, 2021)
 
 def test_instance_creation(bicimad_instance):
-    """
-    Verifica la correcta creación de una instancia de BiciMad y la asignación de atributos de mes y año.
-    """
+    """Verifica la correcta creación de una instancia de BiciMad,
+    incluyendo la correcta inicialización de su DataFrame con datos de prueba."""
     assert bicimad_instance is not None
-    assert bicimad_instance._month == 6
-    assert bicimad_instance._year == 2021
+    assert isinstance(bicimad_instance.data, pd.DataFrame)
+    assert isinstance(bicimad_instance.data.index, pd.DatetimeIndex)
+    assert len(bicimad_instance.data) == 2  # Basado en MOCK_CSV_DATA
 
-def test_data_loading(bicimad_instance):
+def test_clean(bicimad_instance):
     """
-    Comprueba que la carga de datos en la instancia de BiciMad genera un DataFrame no vacío.
-    """
-    data = bicimad_instance.data
-    assert isinstance(data, pd.DataFrame)
-    assert not data.empty
-
-def test_clean_data(bicimad_instance):
-    """
-    Verifica que el método 'clean' de la clase BiciMad funcione correctamente,
-    asegurando que los tipos de datos sean los correctos y que no haya valores NaN innecesarios.
+    Prueba el método de limpieza de la clase BiciMad para asegurarse de que maneja adecuadamente los datos nulos o faltantes.
     """
     bicimad_instance.clean()
-    data = bicimad_instance.data
-    assert data['fleet'].dtype == object
-    assert data['idBike'].dtype == object
-    assert data.isna().sum().sum() == 0
+    assert 'NaN' not in bicimad_instance.data.values
 
 def test_resume(bicimad_instance):
     """
-    Verifica que el resumen proporcionado por el método 'resume' de la clase BiciMad sea correcto.
-    Comprueba las claves y algunos valores específicos del resumen.
+    Comprueba la funcionalidad del método resume, asegurándose de que proporcione un resumen adecuado de los datos de uso de bicicletas.
     """
-    resume = bicimad_instance.resume()
-    assert isinstance(resume, pd.Series)
-    expected_keys = {'year', 'month', 'total_uses', 'total_time', 'most_popular_station', 'uses_from_most_popular'}
-    assert set(resume.index) == expected_keys - {'most_popular_station'} | {'most_popular_stations'}
-    assert resume['total_uses'] == 2
-    assert resume['total_time'] == 45 / 60
+    summary = bicimad_instance.resume()
+    assert summary['total_uses'] == 2
+    assert summary['most_popular_stations'] == {'Estación 1', 'Estación 2'}
+    assert summary['uses_from_most_popular'] == 1
 
+def test_day_time(bicimad_instance):
+    """
+    Verifica que el método day_time de BiciMad calcule correctamente el uso total de bicicletas por día.
+    """
+    day_usage = bicimad_instance.day_time()
+    assert len(day_usage) > 0
+
+def test_weekday_time(bicimad_instance):
+    """
+    Evalúa si el método weekday_time proporciona correctamente el uso total de bicicletas por día de la semana.
+    """
+    weekday_usage = bicimad_instance.weekday_time()
+    assert len(weekday_usage) > 0
+    assert 'L' in weekday_usage.index
+
+def test_total_usage_day(bicimad_instance):
+    """
+    Examina si el método total_usage_day de BiciMad cuenta correctamente el uso total de bicicletas por día.
+    """
+    total_usage = bicimad_instance.total_usage_day()
+    assert len(total_usage) > 0
